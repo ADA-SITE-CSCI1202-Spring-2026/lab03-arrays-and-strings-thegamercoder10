2537919814db51eef9b1a778ce2e243fb14177a4
Elnur <thegamercoder19@gmail.com>
2026-02-19 10:38:13 +0400

    Reverse

--- StringUtils.java
+++ StringUtils.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class StringUtils {
 
+    public static String reverse(String s)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int i = s.length()-1; i>=0; i--)  sb.append(s.charAt(i));
+           
+        return sb.toString();
+    }
+
     public static String explode(String s)
     {
         StringBuilder sb = new StringBuilder();
@@ -24,7 +32,7 @@ public class StringUtils {
     {
         Scanner sc = new Scanner(System.in);
         String in = sc.nextLine();
-        
+
         System.out.println(explode(in));       
         System.out.println(sort(in)); 
     }
