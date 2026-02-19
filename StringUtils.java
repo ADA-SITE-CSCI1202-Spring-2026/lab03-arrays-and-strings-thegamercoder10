69a53d5d12e6c4f7b53d3d152d508b3de63184d6
Elnur <thegamercoder19@gmail.com>
2026-02-19 10:55:23 +0400

    Mixed Strings

--- StringUtils.java
+++ StringUtils.java
@@ -38,16 +38,38 @@ public class StringUtils {
         return Arrays.equals(sc1, sc2);
     }
 
+    public static String mixedString(String s)
+    {
+        StringBuilder sb = new StringBuilder();
+        String ws[] = s.split(" ");
+        for(String w : ws)
+        {
+            sb.append(w.charAt(w.length()-1));
+            if(w.length()>1)
+            {
+            sb.append(w.substring(1,w.length()-1));
+            sb.append(w.charAt(0));
+            }
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
         String in1 = sc.nextLine();
         String in2 = sc.nextLine();
+        String in3 = sc.nextLine();
+
 
         System.out.println(reverse(in1));
         System.out.println(explode(in1));       
         System.out.println(sort(in1)); 
         System.out.println(isAnagram(in1, in2));
+        System.out.println(mixedString(in3));
+
+
 
     }
 }
